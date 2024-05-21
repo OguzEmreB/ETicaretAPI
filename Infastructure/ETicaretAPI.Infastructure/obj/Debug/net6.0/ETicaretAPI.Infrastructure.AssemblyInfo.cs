4e1982288801f0458932bcc03a3ecc83f3d6a59b
OguzEmre <oguzemre.b@gmail.com>
2024-05-22 01:35:19 +0300

    Authentication - Jwt Token - Edit

--- Infastructure/ETicaretAPI.Infastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
+++ Infastructure/ETicaretAPI.Infastructure/obj/Debug/net6.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c5bcb4aaed1d575c40b5f5133d774db3df03d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1e8ac3c3ba1a1fee2ab88b75292d7d64e69e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
